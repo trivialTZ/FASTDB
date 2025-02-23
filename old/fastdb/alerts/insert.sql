6394f3cf3844f0692c0d85cd39c6b8b3441a8b9c
Rob Knop (Nersc) <raknop@lbl.gov>
2025-02-23 09:36:56 -0800

    Remove old directory; it's in the git main history.

--- old/fastdb/alerts/insert.sql
+++
@@ -1,3 +0,0 @@
-insert into broker_classifier (broker_name,broker_version,classifier_name,classifier_params,insert_time) values ('ALeRCE','1.1.0','TinkyWinkyClassifier','1.1.0','2023-11-28 14:53:00');
-insert into broker_classifier (broker_name,broker_version,classifier_name,classifier_params,insert_time) values ('ALeRCE','5.0.0','BaltoClassifier','5.0.0','2023-11-28 14:53:00');
-insert into last_update_time (last_update_time) values ('2023-01-01 00:00:00+00');
