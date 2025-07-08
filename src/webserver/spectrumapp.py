9d98f2e638ef00bd9ac319167cf60aac2f260a72
Rob Knop <raknop@lbl.gov>
2025-07-08 06:42:26 -0700

    small fix

--- src/webserver/spectrumapp.py
+++ src/webserver/spectrumapp.py
@@ -96,7 +96,7 @@ class WhatSpectraAreWanted( BaseView ):
                                           - datetime.timedelta( days=detected_in_last_days ) ).mjd
 
         if 'no_spectra_in_last_days' in data.keys():
-            no_spectra_in_last_days = int( data['not_observed_in_last_days'] )
+            no_spectra_in_last_days = int( data['no_spectra_in_last_days'] )
         else:
             no_spectra_in_last_days = 7
         nospecsince = astropy.time.Time( datetime.datetime.now()
