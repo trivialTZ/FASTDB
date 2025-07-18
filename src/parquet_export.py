fde40a17ddb88297af0927dae8eb2c77d9a32f1a
Konstantin Malanchev <hombit@gmail.com>
2025-07-18 15:45:43 -0500

    Deselect UUID columns

--- src/parquet_export.py
+++ src/parquet_export.py
@@ -6,15 +6,22 @@ from db import DB
 def create_diaobject_sources_view(connection, procver):
     """Create a materialized view joining ``diaobject`` and ``diasource``."""
 
-    # cursor.execute(
-    #     "DROP MATERIALIZED VIEW IF EXISTS diaobject_with_sources"
-    # )
+    # We select the same procver for both object and source, but this should be changed when the provinance
+    # system matures.
+    # We generally want all the columns to be there, and probably join with more tables, but for now we keep
+    # it simple. Also, there is a bug in pg_parquet, which prevents us from using UUID columns:
+    # https://github.com/CrunchyData/pg_parquet/issues/140
     with connection.cursor() as cursor:
         cursor.execute(
             sql.SQL(
                 """
                 CREATE TEMPORARY TABLE diaobject_with_sources AS
-                    SELECT o.*, ds.diasources
+                    SELECT o.diaobjectid, o.processing_version, o.radecmjdtai, o.validitystart, o.validityend,
+                           o.ra, o.raerr, o.dec, o.decerr, o.ra_dec_cov, o.nearbyextobj1, o.nearbyextobj1sep,
+                           o.nearbyextobj2, o.nearbyextobj2sep, o.nearbyextobj3, o.nearbyextobj3sep,
+                           o.nearbylowzgal, o.nearbylowzgalsep, o.parallax, o.parallaxerr, o.pmra, o.pmraerr,
+                           o.pmra_parallax_cov, o.pmdec, o.pmdecerr, o.pmdec_parallax_cov,
+                           ds.diasources
                     FROM diaobject AS o
                     LEFT JOIN (
                         SELECT diaobjectid,
